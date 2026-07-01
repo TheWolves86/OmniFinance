72726f41f6a2bcf9b46de6056d280c7a7cf759a0
TheWolves86 <thewolves86.dev@gmail.com>
2026-07-01 23:53:12 +0600

    50% ui made

--- app/(onboarding)/permissions.tsx
+++ app/(onboarding)/permissions.tsx
@@ -4,12 +4,54 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { useRouter } from 'expo-router'
 
 const PermissionsPage = () => {
+    const [cameraEnabled, setCameraEnabled] = useState(false);
+        const [smsEnabled, setSmsEnabled] = useState(false);
     return (
         <SafeAreaView style={styles.safeArea}>
             <View style={styles.screen}>
                 <View style={styles.header}>
                     <Text style={styles.headerTitle}>A few permissions needed</Text>
                     <Text style={styles.headerDescription}>OmniFinance works fully offline. Permissions stay on your device.</Text>
+                </View>
+                <View style={styles.middleView}>
+                    <View style={styles.permissionContainer}>
+                        <View style={styles.permissionCard}>
+                            <View style={styles.leftSection}>
+                                <View style={styles.iconCircle}>
+                                    <Text>📷</Text>
+                                </View>
+                                <View style={styles.textWrapper}>
+                                    <Text style={styles.permissionTitle}>Camera Access</Text>
+                                    <Text style={styles.permissionDescription}>To scan receipts and extract amounts automatically.</Text>
+                                </View>
+                            </View>
+                            <Switch
+                                value={cameraEnabled}
+                                onValueChange={setCameraEnabled}
+                            />
+                        </View>
+                        <View style={styles.permissionCard}>
+                            <View style={styles.leftSection}>
+                                <View style={styles.iconcircleDark}>
+                                    <Text>💬</Text>
+                                </View>
+                                <View style={styles.textWrapper}>
+                                    <Text style={styles.permissionTitle}>
+                                        SMS Access (Android)
+                                    </Text>
+                                    <Text style={styles.permissionDescription}>
+                                        To read bank alerts and auto-log transactions
+                                    </Text>
+                                </View>
+                            </View>
+                            <Switch
+                            value={smsEnabled}
+                            onValueChange={setSmsEnabled}/>
+                        </View>
+                    </View>
+                    <Text style={styles.infoText}>
+                        You can change these anytime in Settings.
+                    </Text>
                 </View>
             </View>
         </SafeAreaView>
@@ -40,26 +82,65 @@ const styles = StyleSheet.create({
         color: "#666",
         lineHeight: 22
     },
+    middleView: {
+        paddingHorizontal: 20,
+    },
     permissionContainer: {
-
+        marginTop: 40,
+        gap: 18
     },
     permissionCard: {
-
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 18,
+        borderRadius: 18,
+        backgroundColor: "#F8F8F8",
+        
     },
     leftSection: {
-
+        flexDirection: "row",
+        alignItems: "center",
+        flex: 1
     },
     iconCircle: {
-
+        width: 46,
+        height: 46,
+        borderRadius: 23,
+        backgroundColor: "#FFD54F",
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 14
     },
     iconcircleDark: {
-
+        width: 46,
+        height: 46,
+        borderRadius: 23,
+        backgroundColor: "#0A1628",
+        justifyContent: "center",
+        alignItems: "center",
+        marginRight: 14
     },
     permissionTitle: {
-
+        fontWeight: "700",
+        fontSize: 16
     },
     permissionDescription: {
-
+        marginTop: 3,
+        color: "#666",
+        fontSize: 13,
+        lineHeight: 18,
+        flexWrap: 'wrap'
     },
-    
-})
+    textWrapper: {
+        flexShrink: 1,
+        paddingRight: 8
+    },
+    infoText: {
+        marginTop: 30,
+        textAlign: "center",
+        color: "#888",
+        fontSize: 12,
+    }
+});
+//
