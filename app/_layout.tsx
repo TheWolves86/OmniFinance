f817e6158026f733e6b8b9079ba33f9766d0a8ee
TheWolves86 <thewolves86.dev@gmail.com>
2026-07-08 23:55:04 +0600

    Added databse initialisation at start

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
+import { useEffect } from "react";
+import { initializeDatabase } from "@/src/db/init";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const loadDatabase = async () => {
+      try {
+        await initializeDatabase();
+        console.log("Database initialized");
+      } catch (error) {
+        console.log("Database error:", error);
+      }
+    };
+
+    loadDatabase();
+  }, []);
+
   return (
     <Stack
       screenOptions={{
